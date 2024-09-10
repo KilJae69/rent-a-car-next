8f1991d70503293b5ee42c4a53597a6a146eb71a
KilJae69 <adi.toromanovic@outlook.com>
2024-09-10 17:14:33 +0200

    Added static hero section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,12 @@
+// import HeroSection from "@/components/homepage/hero-section";
 
-import HeroSection from "@/components/homepage/hero-section";
+import StaticHeroSection from "@/components/homepage/static-hero-section";
 
 export default function Home() {
   return (
     <>
-      <HeroSection  />
+      {/* <HeroSection  /> */}
+      <StaticHeroSection />
       <section className="container relative z-30 mb-5 mt-[-100px] px-4 xl:px-0"></section>
     </>
   );
---
+++ components/homepage/static-hero-section.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import rotator1 from "@/public/images/rotator_1.webp";
+
+export default function StaticHeroSection() {
+  return (
+    <section className="mx-auto h-[590px] max-w-[1675px] overflow-hidden md:rounded-b-lg lg:h-[665px] 3xl:max-w-[1870px]">
+      <div className="relative size-full px-2 pb-[150px]">
+        <div className="absolute inset-x-0 top-0 z-10 size-full bg-[rgba(1,58,151,0.5)] md:bg-[rgba(1,58,151,0.6)]"></div>
+        <Image
+          src={rotator1}
+          alt={"Hero Image"}
+          className="absolute left-0 top-0 z-0 size-full object-cover"
+          priority
+          placeholder="blur"
+          sizes="
+          (min-width: 1940px) 1870px,
+          (min-width: 1780px) 1675px,
+          (min-width: 380px) calc(93.84vw + 23px),
+          calc(66.67vw + 100px)
+           "
+          fill={true}
+        />
+        <div className="relative z-10 mx-auto flex size-full max-w-[1200px] flex-col items-center justify-end text-center text-white">
+          <h2 className="pb-4 text-2xl font-bold lg:text-4xl 2xl:text-5xl">
+            Lorem ipsum dolor sit amet, consectetur.
+          </h2>
+          <p className="line-clamp-5 overflow-hidden text-ellipsis text-lg font-normal leading-6 2xl:text-xl 2xl:leading-8">
+            Integer vulputate, nibh vel sagittis egestas, dui dolor vehicula
+            dui. Integer vulputate, nibh vel sagittis egestas, dui dolor
+            vehicula dui. Integer vulputate, nibh vel sagittis egestas, dui
+            dolor vehicula dui. Integer vulputate, nibh vel sagittis egestas,
+            dui dolor vehicula dui. Integer vulputate, nibh vel sagittis
+            egestas, dui dolor vehicula dui. Integer vulputate, nibh vel
+            sagittis egestas, dui dolor vehicula dui. Integer vulputate, nibh
+            vel sagittis egestas, dui dolor vehicula dui.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
