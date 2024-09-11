55c1e77c1850be368645d20f23b5277f904edfa0
KilJae69 <adi.toromanovic@outlook.com>
2024-09-11 11:28:03 +0200

    dynamic hero and header

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,18 +1,18 @@
+
 import StaticHeroSection from "@/components/homepage/static-hero-section";
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 
 // import StaticHeroSection from "@/components/homepage/static-hero-section";
 
-// const DynamicHeroSection = dynamic(
-//   () => import("@/components/homepage/hero-section"),
-//   { ssr: false, loading: () => <StaticHeroSection /> }
-// );
-
+const DynamicHeroSection = dynamic(
+  () => import("@/components/homepage/hero-section"),
+  { ssr: false, loading: () => <StaticHeroSection /> },
+);
 export default function Home() {
   return (
     <>
-      {/* <DynamicHeroSection /> */}
-     <StaticHeroSection /> 
+      <DynamicHeroSection  />
+      {/* <StaticHeroSection /> */}
       <section className="container relative z-30 mb-5 mt-[-100px] px-4 xl:px-0"></section>
     </>
   );
