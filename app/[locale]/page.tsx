52472209a159ae39e1f06adefa07177ef4794829
KilJae69 <adi.toromanovic@outlook.com>
2024-09-11 13:49:11 +0200

    dynamic hero section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,15 +1,10 @@
-
-import StaticHeroSection from "@/components/homepage/static-hero-section";
-
-
-
-
+import HeroSection from "@/components/homepage/hero-section";
 
 export default function Home() {
   return (
     <>
       
-      <StaticHeroSection /> 
+      <HeroSection /> 
       <section className="container relative z-30 mb-5 mt-[-100px] px-4 xl:px-0"></section>
     </>
   );
--- components/homepage/hero-section.tsx
+++ components/homepage/hero-section.tsx
@@ -1,20 +1,33 @@
+"use client";
 import dynamic from "next/dynamic";
 
-import HeroSlide, { dummySlideData } from "@/components/homepage/components/hero-slide";
+import HeroSlide, {
+  dummySlideData,
+} from "@/components/homepage/components/hero-slide";
+import { useState } from "react";
+import StaticHeroSection from "./static-hero-section";
 
 // Dynamically import the HeroSlider, with SSR disabled
-const HeroSlider = dynamic(() => import("./components/hero-slider"), {
+const DynamicHeroSlider = dynamic(() => import("./components/hero-slider"), {
   ssr: false, // Disable SSR for client component
+  loading: () => <StaticHeroSection />,
 });
 
 export default function HeroSection() {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <section className="mx-auto h-[590px] max-w-[1675px] overflow-hidden md:rounded-b-lg lg:h-[665px] 3xl:max-w-[1870px]">
-      <HeroSlider>
-        {dummySlideData.map((slide) => (
-          <HeroSlide key={slide.id} item={slide} priority={slide.id === 1} />
-        ))}
-      </HeroSlider>
+    <section
+      className="mx-auto h-[590px] max-w-[1675px] overflow-hidden md:rounded-b-lg lg:h-[665px] 3xl:max-w-[1870px]"
+      onMouseEnter={() => setIsHovered(true)}
+    >
+      {!isHovered && <StaticHeroSection />}
+      {isHovered && (
+        <DynamicHeroSlider>
+          {dummySlideData.map((slide) => (
+            <HeroSlide key={slide.id} item={slide} priority={slide.id === 1} />
+          ))}
+        </DynamicHeroSlider>
+      )}
     </section>
   );
 }
