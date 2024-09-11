0a7883ba690cac36c958f321a12713baed57db3a
KilJae69 <adi.toromanovic@outlook.com>
2024-09-11 13:25:45 +0200

    DestopNav dynamic loading

--- components/layout/main-header.tsx
+++ components/layout/main-header.tsx
@@ -4,13 +4,21 @@ import { cn } from "@/lib/utils";
 import useScrolled from "@/hooks/useScrolled";
 import HeaderLogo from "./helpers/header-logo";
 import { useBoundStore } from "@/store/store";
-import DesktopNav from "./helpers/desktop-nav";
+// import DesktopNav from "./helpers/desktop-nav";
 import MobileMenuToggle from "./helpers/MobileMenuToggle";
 
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+
+import { ChevronRight } from "lucide-react";
+
+const DynamicDesktopNav = dynamic(() => import("./helpers/desktop-nav"), {
+  ssr: false, // Disable SSR for client-side only component
+  loading: () => <StaticNavPlaceholder />,
+});
 
 // Dynamically import MobileMenu
-const DynamicMobileMenu = dynamic(() => import('./mobile-menu'), {
+const DynamicMobileMenu = dynamic(() => import("./mobile-menu"), {
   ssr: false, // Disable server-side rendering for client-side only component
 });
 
@@ -20,6 +28,17 @@ export default function MainHeader() {
   const isReservationWidgetOpen = useBoundStore(
     (state) => state.isReservationWidgetOpen
   );
+
+  // State to control when DesktopNav should be loaded
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  // Load DesktopNav if the user scrolls
+  useEffect(() => {
+    if (isScrolled) {
+      setIsNavVisible(true);
+    }
+  }, [isScrolled]);
+
   return (
     <>
       <header
@@ -40,9 +59,16 @@ export default function MainHeader() {
           )}
         >
           <HeaderLogo isScrolled={isScrolled} />
-          <div className="hidden items-center lg:flex">
-            <DesktopNav isScrolled={isScrolled} />
-            {/* <div className="pl-2"> */}
+          <div
+            className="hidden items-center lg:flex"
+            onMouseEnter={() => setIsNavVisible(true)} // Load DesktopNav on hover
+          >
+            {/* Static Nav Placeholder */}
+            {!isNavVisible && <StaticNavPlaceholder />}
+
+            {/* Dynamically load DesktopNav on scroll or hover */}
+            {isNavVisible && <DynamicDesktopNav isScrolled={isScrolled} />}
+            {/* <div classNameName="pl-2"> */}
             {/*  <ThemeToggler /> */}
             {/* </div> */}
           </div>
@@ -54,5 +80,72 @@ export default function MainHeader() {
       </header>
       {isMobileMenuOpen && <DynamicMobileMenu />}
     </>
+  );
+}
+
+// Simple static placeholder for the initial nav
+
+function StaticNavPlaceholder() {
+  return (
+    <div className="hidden items-center lg:flex">
+      <nav>
+        <ul className="flex">
+          <li className=" relative">
+            <a
+              className="relative block px-4 py-3 text-sm font-bold transition-all ease-in-out hover:text-primary 2xl:py-[50px]"
+              href="/hr"
+            >
+              Home
+            </a>
+          </li>
+          <li className=" relative">
+            <a
+              className="relative block px-4 py-3 text-sm font-bold transition-all ease-in-out hover:text-primary 2xl:py-[50px]"
+              href="/hr/fleet?category=1"
+            >
+              Vehicles
+              <ChevronRight className="absolute bottom-[-10px] left-1/2 size-5 -translate-x-1/2 rotate-90 2xl:bottom-[25px]" />
+            </a>
+          </li>
+          <li className=" relative">
+            <a
+              className="relative block px-4 py-3 text-sm font-bold transition-all ease-in-out hover:text-primary 2xl:py-[50px]"
+              href="/hr/blog"
+            >
+              Blog
+              <ChevronRight className="absolute bottom-[-10px] left-1/2 size-5 -translate-x-1/2 rotate-90 2xl:bottom-[25px]" />
+            </a>
+          </li>
+          <li className="relative">
+            <a
+              className="relative block px-4 py-3 text-sm font-bold transition-all ease-in-out hover:text-primary 2xl:py-[50px]"
+              href="/hr/contact"
+            >
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <nav className="relative z-10 flex max-w-max flex-1 items-center justify-center">
+        <div className="relative">
+          <ul className=" flex flex-1 list-none items-center justify-center space-x-1">
+            <li>
+              <button className=" mt-0 flex h-full w-max flex-col items-center justify-center rounded-none border-l border-gray-200 bg-background p-4 py-2 text-sm font-medium  focus:outline-none dark:border-gray-700 2xl:py-[30px]">
+                <div className="mt-5 text-sm font-bold">EUR</div>
+                <ChevronRight className="relative top-px ml-1 size-5 rotate-90" />
+              </button>
+            </li>
+            <li>
+              <button className="group mt-0 flex h-full w-max flex-col items-center justify-center rounded-none border-l border-gray-200 bg-background p-4 py-2 text-sm font-medium  focus:outline-none dark:border-gray-700 2xl:py-[30px]">
+                <div className="mt-5 text-sm font-bold">Croatian</div>
+
+                <ChevronRight className="relative top-px ml-1 size-5 rotate-90" />
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="absolute left-0 top-full flex justify-center"></div>
+      </nav>
+    </div>
   );
 }
